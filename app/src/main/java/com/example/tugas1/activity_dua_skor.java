1b2b5d1c98a64f315db4d890e9fc0add5fc28fc1
elviaarzaaqa <elviaarzaaqa04@gmail.com>
2022-10-17 22:09:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugas1/activity_dua_skor.java
@@ -0,0 +1,14 @@
+package com.example.tugas1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_dua_skor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dua_skor);
+    }
+}
